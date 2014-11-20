fc4bcb1fb4cb8b7880e0f65b59f967794f296784
olinares <olinares9833@gmail.com>
2014-11-20 15:42:46 -0800

    Added Quotes array on homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,12 @@
 class WelcomeController < ApplicationController
 
   def home
+    @quotes = [
+      ['"Failure is not an option. Everyone has to succeed"', '- Arnold Schwargenegger'],
+      ['"Your time is limited, so don\'t waste it living someone else\'s life."', '- Steve Jobs'],
+      ['"Better Ingredients, Better Pizza"', '- Papa John\'s'],
+    ]
+
   end
   def about
   end
